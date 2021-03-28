cd3cad761c4bee8bb0930dfb729ab464f52b5904
Bryan Martinez <bryanfmr0305@gmail.com>
2021-03-28 12:50:35 -0700

    Prueba Commit

---
+++ build-Proyecto1-Desktop-Debug/CMakeFiles/Proyecto1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto1_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Proyecto1_autogen.dir/ParseCache.txt"
+  "Proyecto1_autogen"
+  "CMakeFiles/Proyecto1.dir/Proyecto1_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Proyecto1.dir/main.cpp.o"
+  "CMakeFiles/Proyecto1.dir/mainwindow.cpp.o"
+  "Proyecto1"
+  "Proyecto1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
